b5e1e5ca684388601f0081bf1b8431448095f538
luvic.visage <gastonthomasmaggi95@gmail.com>
2022-05-23 16:29:55 -0300

    Completado-Mesa 9

--- 01-IntroToCS/homework/homework.js
+++ 01-IntroToCS/homework/homework.js
@@ -2,11 +2,25 @@
 
 function BinarioADecimal(num) {
   // tu codigo aca
-
+  var arr = Array.from(num);
+  let decimal = 0;
+  for (let i = 0; i < arr.length; i++) {
+    decimal = decimal + Math.pow(2, arr.length - i-1) * arr[i];
+  }
+  return decimal;
 }
 
 function DecimalABinario(num) {
   // tu codigo aca
+let numero = num;
+let newArray = [];
+while (numero / 2 > 0) {
+  let resto = numero % 2;
+  newArray.unshift(resto);
+  numero = Math.floor(numero/2);
+}
+
+return newArray.join('');
 
 }
 
